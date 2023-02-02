64b7a3555968c95d47b961895dddf428c18424e2
Yingyang Zhang <yingyang.zhang95@gmail.com>
2023-02-02 10:21:06 -0500

    prepare for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_10_24_181344) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
